05c7169a088fa6bb4433393b302d197b3ca1c769
Kaori Abe <kaori.abe@rakus-partners.co.jp>
2021-08-13 17:41:25 +0900

    add new file

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+//AppコンポーネントからClockコンポーネントの大きさを指定するためclassを付けてある
+<template>
+  <div>
+    <Clock class="clock"/>
+  </div>
+</template>
+
+//AppコンポーネントにimportでClockコンポーネントを読み込み、componentsに登録して使用出来るように
+<script>
+import Clock from "@/components/Clock"
+
+export default {
+  components: {
+    Clock,
+  },
+}
+</script>
+
+//styleにscopedを付けないと全体に適用される
+<style scoped>
+.clock {
+  width: 80%;
+  max-width: 500px;
+  margin: 30px auto;
+}
+</style>
+ 
+<style>
+html {
+  font-size: 62.5%;
+} 
+ 
+body {
+  margin: 0;
+}
+ 
+p {
+  margin: 0;
+}
+</style>
---
+++ test/unit/specs/Clock.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Clock from '@/components/Clock'
+
+describe('Clock.vue', () => {
+  it('表示される日付が正しいかの確認', () => {
+    const Constructor = Vue.extend(Clock)
+    const vm = new Constructor().$mount()
+
+    expect(vm.$el.querySelector('.date').textContent)
+      .toEqual('2021/08/13')
+  })
+})
